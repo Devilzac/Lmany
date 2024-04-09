7f9b9c2bc50169e4beed29e26bd58fc4950c0808
devilzac <shane199274@gmail.com>
2024-04-08 19:51:45 -0500

    forcing style update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title', 'Your Website')</title> 
     <link rel="stylesheet" href="{{ asset('css/addCharacters.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css?v=3') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css?v=4') }}">
     <link rel="stylesheet" href="{{ asset('css/relation.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
