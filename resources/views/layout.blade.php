720625f7de1449adddb8dfe76105f16766c18228
devilzac <shane199274@gmail.com>
2024-04-11 15:22:29 -0500

    wave effect on button

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title', 'Your Website')</title> 
     <link rel="stylesheet" href="{{ asset('css/addCharacters.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css?v=4') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css?v=5') }}">
     <link rel="stylesheet" href="{{ asset('css/relation.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -43,6 +43,7 @@
             <input type="checkbox" name="filterToggle" id="filterToggle" />
             <label class="filterToggle" for="filterToggle">
                 <img src="{{ asset('/images/toggle/shield.webp') }}" alt="">
+                <div class="tapping"></div>
             </label>
             <form action={{ route('character.filtersearch') }} class="d-flex filter-cnt row-wrap" role="search" method="POST">
                 @csrf
